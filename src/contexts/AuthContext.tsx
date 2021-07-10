0d56648daf277855d67d8eb920b1054ee748090f
NearMaick <maick_a_s@msn.com>
2021-07-10 12:05:32 -0300

    feat(web): auto logout

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,5 @@
 import { createContext, ReactNode, useEffect, useState } from 'react'
-import { parseCookies, setCookie } from 'nookies'
+import { destroyCookie, parseCookies, setCookie } from 'nookies'
 import Router  from 'next/router'
 
 import { api } from '../services/api'
@@ -27,6 +27,13 @@ interface AuthProviderProps {
 
 export const AuthContext = createContext({} as AuthContextData)
 
+export function signOut() {
+  destroyCookie(undefined, 'nextauth.token')
+  destroyCookie(undefined, 'nextauth.refreshToken')
+
+  Router.push('/')
+}
+
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>();
   const isAuthenticated = !!user;
@@ -40,7 +47,10 @@ export function AuthProvider({ children }: AuthProviderProps) {
 
         setUser({ email, permissions, roles })
       })
-    }
+      .catch(() => {
+        signOut()
+      })
+    } 
   }, [])
 
   async function signIn({ email, password }: SignInCredentials) {
